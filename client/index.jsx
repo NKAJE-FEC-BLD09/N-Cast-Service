1b1872939e78711cf2dd7aafd48e51869ef46d2a
Your Name <natengelking@gmail.com>
2020-05-01 20:13:23 -0600

    react doc is written

--- client/index.jsx
+++ client/index.jsx
@@ -1,1 +1,21 @@
-//react DOM import react stufffff
+import React from 'react';
+import ReactDOM from 'react-dom';
+//components
+//import $ from 'jquery';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <div>
+        Creation Station
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ server/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser'); 
+const port = 3003;
+
+// app.listen(3003)
