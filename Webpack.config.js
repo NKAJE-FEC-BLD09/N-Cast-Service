a006d1196d0410b2c93db08b98728cccfe21d24c
Your Name <natengelking@gmail.com>
2020-05-04 11:25:59 -0600

    render with live server is working

--- Webpack.config.js
+++ Webpack.config.js
@@ -7,13 +7,13 @@ module: {
     {
         test: [/\.jsx$/],
         exclude: /node_modules/,
-        include: __dirname,
         use: {
         loader: "babel-loader",
         options: { presets: ["@babel/preset-react", "@babel/preset-env"] },
         },
     },
     ],
+
         },
         output: { filename: "bundle.js", path: __dirname + "/client/dist" },
-};
+}; 
--- server/server.js
+++ server/server.js
@@ -4,6 +4,15 @@ const cors = require('cors');
 const bodyParser = require('body-parser'); 
 const port = 3003;
 
+
+app.get('/repos', async (req, res) => {
+  try {
+  res.send('from get');
+  } catch (err) {
+    res.status(404).send(err);
+  }
+})
+
 app.listen(port, () => {
-  console.log('listenting on ' + port);
+  console.log(`listenting on ${port}`);
 })
