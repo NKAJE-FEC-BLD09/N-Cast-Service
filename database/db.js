c6f053698e08cab46eba8600864f034199c23608
Your Name <natengelking@gmail.com>
2020-05-08 16:13:51 -0600

    here ya go kyle

--- database/db.js
+++ database/db.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   "user": "nkaje",
   "host": "localhost",
-  "database": "nkajefec",
+  "database": "postgres",
   "password": "password",
   "port": 5432,
 })
@@ -12,7 +12,7 @@ const pool = new Pool({
 //QUERIES HERE
 const queryArr = [
 'SELECT * FROM movie', 
-'SELECT * FROM actor WHERE movieid =$1', 
+'SELECT * FROM actor WHERE Schema.movieid =$1', 
 'SELECT * FROM character WHERE movieid = $1',
 'SELECT * FROM actordescription WHERE actorid = $1',
 'SELECT * FROM quote WHERE characterid = $1',
@@ -24,6 +24,7 @@ const getQuery = (id) => {
     for (let query of queryArr) {
       return new Promise((resolve, reject) => {
         pool.query(query, [id], (err, result) => {
+          console.log('in query');
           if (err) {
             console.log(err);
             reject(err);
@@ -40,15 +41,15 @@ module.exports = {
 getQuery
 }; 
 
-try {
-let result = await pool.query(queryArr[1], [id], (err, result) => {
-  if (err) {
-    console.log(err)
-    return (err);
-  }
-  console.log('result')
-  res.status(200).send(result.rows);
-})
-} catch (err) {
-  res.status(404).send(err);
-}
+// try {
+// let result = await pool.query(queryArr[1], [id], (err, result) => {
+//   if (err) {
+//     console.log(err)
+//     return (err);
+//   }
+//   console.log('result')
+//   res.status(200).send(result.rows);
+// })
+// } catch (err) {
+//   res.status(404).send(err);
+// }
