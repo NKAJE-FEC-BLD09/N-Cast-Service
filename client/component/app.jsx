6b753171056baa5632d11bd38d0c2b2dbb9c641e
Your Name <natengelking@gmail.com>
2020-05-09 14:09:12 -0600

    set reactDOM to its own file

---
+++ client/component/app.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+//components
+//import $ from 'jquery';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <div>
+        Creation Station
+      </div>
+    )
+  }
+}
+
+export default App;
--- client/index.jsx
+++ client/index.jsx
@@ -1,21 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//components
-//import $ from 'jquery';
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render () {
-    return (
-      <div>
-        <h2>Creation Station</h2>
-      </div>
-    )
-  }
-}
-
+import App from './component/app.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
